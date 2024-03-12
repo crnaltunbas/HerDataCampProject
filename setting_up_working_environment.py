ef442b2b4a53649fa553750d482bc90f48ba1450
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-03-12 15:25:26 +0300

    Sayılar ve Karakter dizileri gösterildi.

--- setting_up_working_environment.py
+++ setting_up_working_environment.py
@@ -1,2 +1,11 @@
+#  Sayılar(Numbers) ve Karakter Dizileri(Strings)
+#  String tırnak işareti ile gösterdiğimiz ifadelerdir.
+#  İnteger 0,1,2,3,4,5,6,7,8,9... gibi sayılardır.
+#  Float ondalıklı sayılardır. 9.2 gibi
+print(9)
+type(9.5)
 print("Hello world!")
 print("Hello AI Ero")
+type("naber cano?")
+#  type() foksiyonu değişkenimizin, verimizin hangi veri tipinde olduğunu gösterir.
+
