bc7fb6c29f19ca4e80e7c1af96209020bc16e2ca
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-03-12 16:39:14 +0300

    Conda ile paket yönetimi için önemli komutlar açıklandı.

--- setting_up_working_environment.py
+++ setting_up_working_environment.py
@@ -19,3 +19,36 @@ c=10
 a*c
 a*20
 a-c
+
+#  Virtual Environment (Sanal Ortam) ve Paket Yönetimi (Package Manager)
+#  Conda hem sanal ortam aracı hem de paket yönetimi aracıdır.
+#  Bilgisayardaki sanal ortamın listelenmesi için aşağıdaki komut kullanılır.
+#  conda env list
+#  Bu komutlar terminalde kullanılır.Sanal ortam oluşturmak için aşağıdaki komut kullanılır.
+#  conda create -n kendi sanal ortamımızın ismi yazılır.
+#  Oluşturduğumuz sanal ortama gitmek yani aktif etmek için aşğıdaki komut kullanılır.
+#  conda activate mynenv
+#  Girdiğimiz sanal ortamdan çıkmak için aşağıdaki komut kullanılır.
+#  conda deactivate
+#  Ortam içerisindeki paketleri görmek için aşağıdaki komut kullanılır.
+#  conda list
+#  Paket Yüklemek için aşağıdaki komut kullanılır.
+#  conda install numpy
+#  Aynı anda birden fazla paket yüklemek için paketler arasında bir boşluk bırakarak yazılır.
+#  Bunun için aşağıdaki komut kullanılır.
+#  conda install numpy scipy pandas
+#  Paket silmek için aşağıdaki komut kullanılır.
+#  conda remove paket ismi
+#  Belirli bir versiyona göre paket yüklemek için aşağıdaki komut kullanılır.
+#  conda install numpy=1.26.3
+#  Paket yükseltmek için aşağıdaki komut kullanılır.
+#  conda upgrade conda
+#  Tüm paketlerin yükseltilmesi için aşağıdaki komut kullanılır.
+#  conda upgrade -all
+
+
+
+
+
+
+
